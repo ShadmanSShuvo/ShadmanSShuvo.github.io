faa97385bb936cba8a7fa151f3760bdcedfdfa1e
Md. Shadman Shahriyar Shuvo <shuvobuet23@gmail.com>
2025-04-25 20:46:22 +0600

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,6 +16,19 @@
   `;
     let prevUrl = "/research";
     let nextUrl = "/achievements";
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/680b9f12bb1753191a4dd4dc/1ipmmaov8';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </script>
 
 <div class="flex flex-col items-center space-y-4 md:space-y-8">
