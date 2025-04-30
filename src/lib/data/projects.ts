f07b0527e448d32f2a28f8679b3c26a17a95698a
Md. Shadman Shahriyar Shuvo <shuvobuet23@gmail.com>
2025-04-30 15:37:16 +0600

    Update projects.ts

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -6,32 +6,74 @@ import c_compiler_poster from "$lib/images/c_compiler_poster.webp";
 export const projects = [
     {
         title: "Flappy Bird Clone Game",
-        imageUrl: buet_cse_courseworks_poster,
+        imageUrl: null,
         githubUrl: "https://github.com/ShadmanSShuvo/FlappyBird",
         youtubeUrl: null,
         tags: ["BUET CSE", "Courseworks"],
         year: "2024",
         description: "This repository contains the links to the CSE 102 iGraphics Project of L1-T1."
     },
-    {
-        title: "CSE 102 Courseworks",
-        imageUrl: c_compiler_poster,
-        githubUrl: "https://github.com/ShadmanSShuvo/CSE102-coursework",
-        youtubeUrl: null,
-        tags: ["C"],
-        year: "2024",
-        description: "This repository contains the links to all my courseworks for CSE 102 sessional course of L1-T1."
-    },
+
     {
         title: "National Camp Journal",
-        imageUrl: taskflow_poster,
+        imageUrl: null,
         githubUrl: "https://github.com/ShadmanSShuvo/Nat-Camp-Journal",
         youtubeUrl: null,
         tags: ["LaTeX", "Math", "Olympiad", "Problem Solving"],
         year: "2021",
         description:
             "This is a LaTeX project that contains the journal of the National Olympiad Camp 2021. It contains the lectures, problems and solutions of the camp."
-        },
+    },
+
+    {
+        title: "CSE 102 (C) Courseworks",
+        imageUrl: null,
+        githubUrl: "https://github.com/ShadmanSShuvo/CSE102-coursework",
+        youtubeUrl: null,
+        tags: ["C"],
+        year: "2024",
+        description: "This repository contains the links to all my courseworks for CSE 102 Sessional course of L1-T1."
+    },
+
+    {
+        title: "CSE 105 (DSA) Courseworks",
+        imageUrl: null,
+        githubUrl: "https://github.com/ShadmanSShuvo/CSE105",
+        youtubeUrl: null,
+        tags: ["C", "C++", "Data Structures", "Algorithms"],
+        year: "2025",
+        description: "This repository contains the links to all my courseworks for CSE 105 (DSA) Theory course of L1-T2."
+    },
+
+    {
+        title: "CSE 106 (DSA) Courseworks",
+        imageUrl: null,
+        githubUrl: "https://github.com/ShadmanSShuvo/CSE106",
+        youtubeUrl: null,
+        tags: ["C", "C++", "Data Structures", "Algorithms"],
+        year: "2025",
+        description: "This repository contains the links to all my courseworks for CSE 106 (DSA) Sessional course of L1-T2."
+    },
+
+    {
+        title: "CSE 107 (OOP) Courseworks",
+        imageUrl: null,
+        githubUrl: "https://github.com/ShadmanSShuvo/CSE107",
+        youtubeUrl: null,
+        tags: ["C", "C++", "Java", "OOP"],
+        year: "2025",
+        description: "This repository contains the links to all my courseworks for CSE 107 (OOP) Theory course of L1-T2."
+    },
+
+    {
+        title: "CSE 108 (OOP) Courseworks",
+        imageUrl: null,
+        githubUrl: "https://github.com/ShadmanSShuvo/CSE108",
+        youtubeUrl: null,
+        tags: ["C", "C++", "Java", "OOP"],
+        year: "2025",
+        description: "This repository contains the links to all my courseworks for CSE 108 (OOP) Sessional course of L1-T2."
+    },
 
 ];
 
